a0a2632bde7f1e7006ad30dd742a21c0ec5f8ba3
Alex Grafen <alex.grafen@gmail.com>
2024-07-13 14:52:13 +0100

    feat: DTO for updating tags on a message

--- src/message/models/message.dto.ts
+++ src/message/models/message.dto.ts
@@ -6,6 +6,7 @@ import {
   registerEnumType,
 } from '@nestjs/graphql';
 import { ObjectID } from 'mongodb';
+import { Tag } from '../../conversation/models/CreateChatConversation.dto';
 
 export enum GifType {
   Gif = 'gif',
@@ -170,6 +171,21 @@ export class ReactionDto {
 
   @Field(() => String)
   reactionUnicode: string;
+
+  @Field(() => ObjectID)
+  messageId: ObjectID;
+
+  @Field(() => ObjectID)
+  conversationId: ObjectID;
+}
+
+@InputType()
+export class UpdateTagsMessageDto {
+  @Field(() => [ObjectID])
+  tags: Tag[];
+
+  @Field(() => ObjectID)
+  userId: ObjectID;
 
   @Field(() => ObjectID)
   messageId: ObjectID;
